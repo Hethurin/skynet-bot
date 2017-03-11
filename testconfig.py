bb305f86f9494c2c15e03fc328fe1d3df28110e7
Hethurin <csgoshadow@datavalue.hethurin.com>
2017-03-11 15:59:08 -0500

    Now image send is working for sure

--- testconfig.py
+++ testconfig.py
@@ -4,12 +4,21 @@ from config import SkynetConfig
 
 class SkynetConfigTestCase(unittest.TestCase):
 
-    def test_reading_level_information(self):
+    def test_reading_text_level_information(self):
         conf = SkynetConfig()
         conf.read_config('testconfig.ini')
 
         expected_data = ('text', 'question0', 'answer0', '0')
         lvl_data = conf.get_level_info('level0')
+
+        self.assertTupleEqual(expected_data, lvl_data)
+
+    def test_reading_image_level_information(self):
+        conf = SkynetConfig()
+        conf.read_config('testconfig.ini')
+
+        expected_data = ('image', 'question2', 'answer2', '2', 'question2.jpg')
+        lvl_data = conf.get_level_info('level1')
 
         self.assertTupleEqual(expected_data, lvl_data)
 
